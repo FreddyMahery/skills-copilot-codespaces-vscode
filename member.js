e2064b60795b4d3345df104530c71b9ce98dff37
FreddyMahery <158273152+FreddyMahery@users.noreply.github.com>
2024-12-27 14:33:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    const member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        // Method
+        showSkills: function() {
+            this.skills.forEach(skill => {
+                console.log(`${this.name} knows ${skill}`);
+            });
+        }
+    };
+    member.showSkills();
+}
